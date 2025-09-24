2f3d5de033f59dfba364f855b463815a67ef4966
williamzav <144736030+williamzav@users.noreply.github.com>
2025-09-24 10:31:55 -0600

    Update Conexion.php
    servidor

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,9 +2,9 @@
     class Conexion{
         public function conectar(){
             $host="localhost";
-            $usuario="root";
-            $password="";
-            $base="agenda";
+            $usuario="backend";
+            $password="backend2025";
+            $base="william_agenda";
             $conexion=mysqli_connect(
                 $host,$usuario,$password,$base
             );
