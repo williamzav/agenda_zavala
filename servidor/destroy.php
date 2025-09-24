5d585d655025963d2ecb6e1c92251d2ce4e9a63b
williamzav <144736030+williamzav@users.noreply.github.com>
2025-09-24 13:36:01 -0600

    Update destroy.php
    destroy

--- servidor/destroy.php
+++ servidor/destroy.php
@@ -1,11 +1,22 @@
-<?php
-include "../clases/Crud.php";
-$id=$_GET['id'];
-$crud=new Crud();
 
-if($crud->destroy($id)){
-    header("location:../index.php");
-}else{
-    echo "Fallo al eliminar";
-}
-?>
+<?php 
+    include "../clases/Crud.php";
+    $id = $_GET['id'];
+    $crud = new Crud();
+
+    // Obtener la foto antes de eliminar el contacto
+    $foto = $crud->get_foto_contacto($id);
+
+
+    // Eliminar primero la foto de la base de datos (por restricción de clave foránea)
+    $crud->eliminar_foto_contacto($id);
+
+    if($crud->destroy($id)){
+        // Eliminar la foto física si existe
+        if ($foto && file_exists($foto["ruta"])) {
+            unlink($foto["ruta"]);
+        }
+        header("location:../index.php");
+    } else {
+        echo "Fallo al eliminar!!";
+    }
